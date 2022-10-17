8afcba1cd157f6c595b273611569095a9597bf47
maceyjshort <macey.walker1991@gmail.com>
2022-10-17 11:02:11 -0500

    Add useState to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import ButtonCounter from "./components/ButtonCounter";
 
+const buttons = [
+  {
+    txt: "Button 1",
+  },
+  {
+    txt: "Button 2",
+  },
+  {
+    txt: "Button 3",
+  },
+];
+
 function App() {
+  const state = {};
   return (
     <>
       <h1 className="mt-8 text-center uppercase text-red-500">
